a2d132824551ff286301f6f9eafd7be1f0dbeaab
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2024-04-02 22:03:05 -0300

    removes h2 connections, adds migrations, connects with HSQLDB locally

---
+++ src/main/kotlin/rodrigoschaer/conferencemanager/ConferenceManagerApplication.kt
@@ -0,0 +1,11 @@
+package rodrigoschaer.conferencemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConferenceManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConferenceManagerApplication>(*args)
+}
---
+++ src/test/kotlin/rodrigoschaer/conferencemanager/ConferenceManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package rodrigoschaer.conferencemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConferenceManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
